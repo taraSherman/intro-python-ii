2f9b3be01d673596a56f04c8570f2cdce2996797
Tara Sherman <taradelin@gmail.com>
2020-09-09 20:58:07 -0400

    From current_room to room in player attributes

--- src/player.py
+++ src/player.py
@@ -2,9 +2,9 @@
 # currently.
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, room):
         self.name = name
-        self.current_room = current_room
+        self.room = room
 
     def __str__(self):
         return f'{self.name}'
